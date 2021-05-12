abf60c22a9e586412d6ed19f2b2ad7e1fe37b7a6
riley <16313469+karateking007@users.noreply.github.com>
2021-05-11 19:56:59 -0500

    Added multi-line chart functionality

--- app.js
+++ app.js
@@ -1,35 +1,62 @@
-var ctx = document.getElementById('graph');
-var graph = new Chart(ctx, {
+const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'];
+
+const data = {
+    labels: labels,
+    datasets: [
+        {
+            label: '${BitCoin}',
+            data: [20, 50, 10, 20, 70, 50, 90],
+            borderColor: 'rgb(255, 0, 0)',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            yAxisID: 'y',
+        },
+        {
+            label: '${S&P 500}',
+            data: [50, 10, 15, 25, 10, 50, 70],
+            borderColor: 'rgb(0, 0, 255)',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            yAxisID: 'y1',
+        }
+    ]
+};
+
+const config = {
     type: 'line',
-    data: {
-        labels: ['blue', 'green', 'yellow', 'purple', 'pink', 'orange'],
-        datasets: [{
-            label: '${`BitCoin`}',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'blue',
-                'green',
-                'yellow',
-                'purple',
-                'pink',
-                'orange'
-            ],
-            borderColor: [
-                'rgba(0, 0, 0, 0)',
-                'rgba(0, 0, 0, 0)',
-                'rgba(0, 0, 0, 0)',
-                'rgba(0, 0, 0, 0)',
-                'rgba(0, 0, 0, 0)',
-                'rgba(0, 0, 0, 0)'
-            ],
-            borderWidth: 0
-        }]
-    },
+    data: data,
     options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false,
+        },
+        stacked: false,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Line Chart - Multi Axis'
+            }
+        },
         scales: {
             y: {
-                beginAtZero: true
-            }
+                type: 'linear',
+                display: true,
+                position: 'left',
+            },
+            y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+
+                // grid line settings
+                grid: {
+                    drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
+            },
         }
-    }
-});
+    },
+};
+
+var myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+);
