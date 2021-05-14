e2aa0dfe94b1ae72aac97fa989e84c3a3beffdaa
riley <16313469+karateking007@users.noreply.github.com>
2021-05-13 21:16:22 -0500

    Added months algorithm and array to stockAPI

--- stockAPI.js
+++ stockAPI.js
@@ -3,6 +3,11 @@ function fetchData() {
     let stockSymbol = 'GSPC';
     let API_Call = `https://api.twelvedata.com/time_series?symbol=${stockSymbol}&exchange=NYSE&interval=1month&outputsize=12&apikey=${API_KEY}`;
     let monthSeries = [];
+    const datesObject = { "01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun", "07": "Jul", "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec", };
+    var workingMonthArr = [];
+    var workingMonth = "";
+    var workingMonthSplitArr = [];
+    var dateNums;
 
     fetch(API_Call)
         .then(
@@ -17,9 +22,15 @@ function fetchData() {
             }
 
             const seriesDates = monthSeries.map(dates => dates.datetime);
-
-
             console.log(seriesDates);
+
+            for (let i = 0; i < seriesDates.length; i++) {
+                workingMonthSplitArr = seriesDates[i].split("");
+                dateNums = workingMonthSplitArr[5].concat(workingMonthSplitArr[6]);
+                workingMonthArr.push(datesObject[dateNums]);
+            }
+            console.log(workingMonthArr);
+
         })
 }
 
