7836626ef644fe215dccd78d24f767f3cc00f453
Riley <rileylitchfield95@gmail.com>
2022-10-21 13:18:48 -0500

    Cleaned up code

--- API.js
+++ API.js
@@ -1,5 +1,3 @@
-
-<script src="apikey.js" type="module"></script>
 import API_KEY from "./apikey";
 
 function fetchData() {
@@ -9,8 +7,7 @@ function fetchData() {
     let stockSymbol = changeStockValue;
     let stock_API_Call = `https://api.twelvedata.com/time_series?symbol=${stockSymbol}&interval=${timeInterval}&outputsize=${outputSize}&apikey=${API_KEY}`;
 
-    let stockResObj = [];
-    let cryptoResObj = [];
+    let stockResObj = [], cryptoResObj = [];
     const datesObject = { "01": "Jan", "02": "Feb", "03": "Mar", "04": "Apr", "05": "May", "06": "Jun", "07": "Jul", "08": "Aug", "09": "Sep", "10": "Oct", "11": "Nov", "12": "Dec" };
 
     let stockWorkingMonthArr = [], cryptoWorkingMonthArr = [], cryptoWorkingYearArr = [], combinedMonthYear = [];
