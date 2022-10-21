d54bb8c145f2cf1152e7752c0376c5be46b16260
Riley <rileylitchfield95@gmail.com>
2022-10-21 14:04:53 -0500

    Cleaned up code

--- API.js
+++ API.js
@@ -19,9 +19,9 @@ function fetchData() {
     fetch(stock_API_Call)
         .then(
             function (response) {
-                let jsonData = response.json();
-                if (jsonData['status' == 'ok']) {
-                    return jsonData;
+                let stockJson = response.json();
+                if (stockJson['status'] == 'ok') {
+                    return stockJson;
                 } else {
                     alert('Error pulling data. Please try back later.');
                 }
